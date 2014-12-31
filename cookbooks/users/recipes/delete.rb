2c746a733c24754bf8871cf96ed5b1d6dfd40ba4
d4nte <ubuntu@ip-172-31-23-148.us-west-2.compute.internal>
2014-12-31 08:28:44 +0000

    ignore vim swp files. add ssh pub key

--- cookbooks/users/recipes/delete.rb
+++ cookbooks/users/recipes/delete.rb
@@ -1,5 +1,17 @@
 ## Used for testing only
 
+username = "dante"
+userhome = "/home/" + username 
+
+# delete ssh pub keys
+file userhome + "/.ssh/authorized_keys" do
+  action :delete
+end
+
+directory userhome + "/.ssh" do
+  action :delete
+end
+
 # delete users
 user "dante" do
   supports :manage_home => true
