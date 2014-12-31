5040e5d9a464f460dc8de362c4fc6aafe1fce918
d4nte <ubuntu@ip-172-31-23-148.us-west-2.compute.internal>
2014-12-31 08:21:01 +0000

    moving files to proper folder

--- cookbooks/users/recipes/default.rb
+++ cookbooks/users/recipes/default.rb
@@ -1,3 +1,4 @@
+
 # Create groups
 group "kollokusers" do
   gid "2000"
@@ -9,11 +10,14 @@ group "kolloksudoers" do
   action :create
 end
 
+username = "dante"
+userhome = "/home/" + username 
+
 # Create users
-user "dante" do
+user username do
   uid "2000"
   gid "kollokusers"
-  home "/home/dante"
+  home userhome
   shell "/bin/bash"
   supports :manage_home => true
   action :create 
@@ -21,6 +25,21 @@ end
 
 # Make the kolloks sudoers
 group "kolloksudoers" do
-  members ['dante']
+  members [ username ]
   action :modify
+end
+
+# Add the ssh pub keys
+directory userhome + "/.ssh" do
+  owner username
+  group 'kollokusers'
+  mode '0700'
+  action :create
+end
+
+cookbook_file userhome + "/.ssh/authorized_keys" do
+  source username + "/ssh.pub"
+  mode 0600
+  owner username
+  group "kollokusers"
 end
